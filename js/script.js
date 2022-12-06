937d10fdc8069896cc493fc1dc0e960cd3e67414
KovAllaptop <koval4ykoo1@gmail.com>
2022-12-06 23:15:30 +0200

    add db

--- js/script.js
+++ js/script.js
@@ -282,6 +282,9 @@ window.addEventListener('DOMContentLoaded', () => {
                 prevModalDialog.classList.remove('hide');
                 modalCloseFunc();
             }, 4000);
-        };
-        
+        }
+
+    fetch('http://localhost:3000/menu')
+            .then(data => data.json())
+            .then(res => console.log(res))
 });
